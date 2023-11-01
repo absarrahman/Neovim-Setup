39018036513365b8bf27fdd71c954013cc6e7bdc
absarrahman <raman040635@gmail.com>
2023-11-01 08:21:07 +0000

    Add remaps and indent plugins/lsp.lua
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lua/absarrahman/core/remap.lua
+++ lua/absarrahman/core/remap.lua
@@ -2,3 +2,23 @@ print("Remap running")
 vim.g.mapleader = " "
 vim.o.timeoutlen = 2000
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
