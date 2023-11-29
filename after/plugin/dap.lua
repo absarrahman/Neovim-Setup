11088ed78adc5d1d7e82ebcc0cde6b8ff67c8cfb
absarrahman <raman040635@gmail.com>
2023-11-29 14:30:33 +0600

    Trigger dapui
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,5 @@
 local dap =  require("dap")
+local dapui = require("dapui")
 
 vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
 vim.keymap.set("n", "<F10>", ":lua require'dap'.set_over()<CR>")
@@ -48,4 +49,16 @@ dap.configurations.dart = {
 
 }
 
-require("dapui").setup()
+dapui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
