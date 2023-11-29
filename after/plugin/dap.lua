2a953cbf2dab089a964f0c02bdb616058fe5e222
absarrahman <raman040635@gmail.com>
2023-11-29 16:43:08 +0600

    Add rust debugger
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -9,6 +9,8 @@ vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
 vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 vim.keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
 vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+vim.keymap.set("n", "<leader>op", ":lua require'dapui'.open()<CR>")
+vim.keymap.set("n", "<leader>cp", ":lua require'dapui'.close()<CR>")
 
 dap.adapters.dart = {
     type = "executable",
@@ -53,12 +55,4 @@ dapui.setup()
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
-  end
-
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
+end
--- lua/absarrahman/plugin/lsp.lua
+++ lua/absarrahman/plugin/lsp.lua
@@ -14,6 +14,36 @@ return {
             {'hrsh7th/cmp-nvim-lsp'},
         }
     },
+    {
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+        dependencies = "neovim/nvim-lspconfig",
+        config = function ()
+            local rt = require("rust-tools")
+            local mason_registry = require("mason-registry")
+
+            -- Need codelldb dap from mason
+            local codelldb = mason_registry.get_package("codelldb")
+            local extension_path = codelldb:get_install_path() .. "/extension/"
+            local codelldb_path = extension_path .. "adapter/codelldb"
+            local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
+            rt.setup({
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                server = {
+                    on_attach = function(_, bufnr)
+                        -- Hover actions
+                        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+                        -- Code action groups
+                        vim.keymap.set("n", "<Leader>vca", rt.code_action_group.code_action_group, { buffer = bufnr })
+                    end,
+                },
+                dap = {
+                    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+                },
+            })
+        end
+    },
     -- Commenting out
     {
         'numToStr/Comment.nvim',
