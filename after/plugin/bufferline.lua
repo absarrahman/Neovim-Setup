b803729dbd806ac9f3be3b56abd71572bdd71316
absarrahman <raman040635@gmail.com>
2023-12-22 19:38:35 +0600

    Add bufferline
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ after/plugin/bufferline.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>bfp", ":BufferLinePick<CR>")
+vim.keymap.set("n", "<leader>bfco", ":BufferLineCloseOthers<CR>")
---
+++ lua/absarrahman/plugin/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = "BufReadPre",
+    config = true,
+    opts = {
+        options = {
+            mode = "buffers",
+            always_show_bufferline = true,
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "Project Explorer",
+                    text_align = "left",
+                    separator = true,
+                },
+            },
+        },
+    }
+}
