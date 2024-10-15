7ed8b0f96334938034427b557e57d1fdddd35d0b
absarrahman <raman040635@gmail.com>
2024-10-14 21:36:53 -0500

    feat: setup python debugger
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lua/absarrahman/plugin/dap.lua
+++ lua/absarrahman/plugin/dap.lua
@@ -13,4 +13,15 @@ return {
             "mfussenegger/nvim-dap",
         },
     },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function(_, opts)
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            require("dap-python").setup(path)
+        end
+    },
 }
