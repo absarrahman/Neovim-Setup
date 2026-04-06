abb3d9bc874a5423477e53130b43332e86448322
absarrahman <raman040635@gmail.com>
2026-04-06 15:25:40 -0500

    fix: update telescope version
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lua/absarrahman/plugin/telescope.lua
+++ lua/absarrahman/plugin/telescope.lua
@@ -1,4 +1,9 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
 }
